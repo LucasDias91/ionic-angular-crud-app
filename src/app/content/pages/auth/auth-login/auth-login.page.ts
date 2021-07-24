7863d0393d70761d84ff2da9e532f2dc6c57e25c
Lucas Dias <lucas.dias@magically.com.br>
2021-07-24 16:59:24 -0300

    login responsive

--- src/app/content/pages/auth/auth-login/auth-login.page.ts
+++ src/app/content/pages/auth/auth-login/auth-login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth-login',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-login.page.scss'],
 })
 export class AuthLoginPage implements OnInit {
+  resizeObservable$: Observable<Event>
+  resizeSubscription$: Subscription
+  size: string = '12';
 
   constructor() { }
 
   ngOnInit() {
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+      if(window.screen.height < 401){
+        this.size = '6';
+        return;
+      }
+      this.size = '12';
+    })
+    console.log()
   }
 
+  login() {
+
+  }
+
+  register() {
+
+  }
+
+  resetPassword() {
+    
+  }
+ 
 }
