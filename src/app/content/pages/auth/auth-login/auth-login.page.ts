5130e299832ca59b969a3b89f81e2f93ee6cd71d
Lucas Dias <lucas.dias@magically.com.br>
2021-07-28 21:03:23 -0300

    adj login page

--- src/app/content/pages/auth/auth-login/auth-login.page.ts
+++ src/app/content/pages/auth/auth-login/auth-login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { fromEvent, Observable, Subscription } from 'rxjs';
+import { Login } from 'src/app/core/models/authentication/login';
 
 @Component({
   selector: 'app-auth-login',
@@ -13,27 +15,47 @@ export class AuthLoginPage implements OnInit {
   size: string = '12';
   loadingAfterSubmit: boolean = false;
   errorMsg: string = '';
+  form: FormGroup;
 
   constructor(private navController: NavController,
-    ) { }
+    private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.resizeObservable$ = fromEvent(window, 'resize')
-    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
-      if(window.screen.height < 401){
-        this.size = '6';
-        return;
-      }
-      this.size = '12';
-    })
-    console.log()
+    this.createForm();
+    this.resizeEvent();
   }
 
-  login() {
-   this.loadingAfterSubmit = true;
-   setTimeout(()=>{
-     this.loadingAfterSubmit = false;
-   },3000)
+  createForm() {
+    const patternEmail: string = "^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$";
+    this.form = this.fb.group({
+      username: [null, Validators.compose([Validators.required, Validators.pattern(patternEmail)])],
+      password: [null, Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(10)])]
+    })
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const login = this.prepareLogin();
+    console.log(login)
+
+  }
+
+  prepareLogin() {
+    const login = new Login();
+    login.grant_type = 'password';
+    login.username = this.form.get('username').value;
+    login.password = this.form.get('password').value;
+    return login;
+  }
+
+  login(login: Login) {
+    this.loadingAfterSubmit = true;
+    setTimeout(() => {
+      this.loadingAfterSubmit = false;
+    }, 3000)
   }
 
   register() {
@@ -41,7 +63,19 @@ export class AuthLoginPage implements OnInit {
   }
 
   resetPassword() {
-    
+
   }
- 
+
+  resizeEvent() {
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(evt => {
+      if (window.screen.height < 401) {
+        this.size = '6';
+        return;
+      }
+      this.size = '12';
+    });
+  }
+
+
 }
